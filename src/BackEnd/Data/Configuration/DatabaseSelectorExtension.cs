a5f1f38e22e228c144bfe62e50ebbf6703ec7ea6
Karollainny Teles <karollainny@aqua.com.br>
2025-07-06 19:22:08 -0300

    Uncomment

--- src/BackEnd/Data/Configuration/DatabaseSelectorExtension.cs
+++ src/BackEnd/Data/Configuration/DatabaseSelectorExtension.cs
@@ -11,15 +11,15 @@ public static class DatabaseSelectorExtension
 {
     public static void AddDatabaseSelector(this WebApplicationBuilder builder)
     {
-        //if (builder.Environment.IsDevelopment())
-        //{
-        //    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        //        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionLite")));
-        //}
-        //else
-        //{
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionLite")));
+        }
+        else
+        {
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-       // }
+        }
     }
 }
