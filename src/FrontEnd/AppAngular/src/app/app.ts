e4e4207e7a11e3845feec380395ef120ad094084
Rafael Secco <raf.secco@gmail.com>
2025-07-21 23:13:31 -0300

    feat: add categoria model and implement footer, home, and menu components
    - Created Categoria interface for category representation.
    - Implemented FooterComponent with a simple footer layout.
    - Developed HomeComponent to display products with details and images.
    - Added MenuComponent for navigation with a brand link.
    - Defined Produto interface for product representation.
    - Implemented ProdutoService to fetch products from the API.
    - Created HTML templates for footer, home, and menu components.

--- src/FrontEnd/AppAngular/src/app/app.ts
+++ src/FrontEnd/AppAngular/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./navegacao/menu/menu-component";
+import { FooterComponent } from './navegacao/footer/footer-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, MenuComponent, FooterComponent],
+  templateUrl: './app.html'
 })
 export class App {
   protected title = 'AppAngular';
